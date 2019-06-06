c226b57f6d2b39be46a8689f56a2eb29c3379178
Barbara Sikora <bs@webundsoehne.com>
2019-06-06 14:46:53 +0200

    Update imports

--- src/app/tasks.service.ts
+++ src/app/tasks.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
+import { Observable } from 'rxjs';
 import  Tasks  from './tasks';
 import { AsyncLocalStorage } from 'angular-async-local-storage';
 
